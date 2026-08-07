27506f7ae7fdc5cfab3d8d8880efcf1628a7dae2
Rhea Govender <u26031371@up.ac.za>
2026-08-07 13:44:30 +0200

    added account registration saving method

--- frmRegisterAccount.cs
+++ frmRegisterAccount.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -20,6 +21,35 @@ namespace GroupProject
         private void btnCreateAccount_Click(object sender, EventArgs e)
         {
             //Create account
+            //button for the Register button when the UI is made, btnCreateAccount
+
+            string username = txtUsername.Text.Trim();
+            string password= txtPassword.Text.Trim();
+
+            if(username.Length==0 || password.Length==0)
+            {
+                MessageBox.Show("Please fill in both username and password fields.", "Validation Errror",MessageBoxButtons.OK,MessageBoxIcon.Error);
+                return;
+            }
+
+            //saving details to text file
+
+            using(System.IO.StreamWriter writer=new System.IO.StreamWriter("users.txt",true))
+            {
+                writer.WriteLine(username + "," + password);
+            }
+
+            MessageBox.Show("Account created successfully!");
+
+            //redirected back to the login page
+            frmLogin loginForm=new frmLogin();
+            this.Hide();
+            loginForm.ShowDialog();
+        }
+
+        CatchBlock(Exception ex)
+        {
+            MessageBox.Show("An error occurred while creatting the account");
         }
 
         private void btnToLogin_Click(object sender, EventArgs e)
