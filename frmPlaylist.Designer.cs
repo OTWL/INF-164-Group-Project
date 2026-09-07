815b944a96639f8e863aad4999d69286dd3df33e
u26271037 <u26271037@tuks.co.za>
2026-09-07 20:01:32 +0200

    Windows media controls (play and stop button) Staged commit

--- frmPlaylist.Designer.cs
+++ frmPlaylist.Designer.cs
@@ -38,17 +38,17 @@
             this.BtnSaveCover = new System.Windows.Forms.Button();
             this.btnBack = new System.Windows.Forms.Button();
             this.dgvSongs = new System.Windows.Forms.DataGridView();
-            this.btnAddSong = new System.Windows.Forms.Button();
-            this.lblNumTracks = new System.Windows.Forms.Label();
-            this.btnDeleteSong = new System.Windows.Forms.Button();
             this.ColTitle = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.colArtist = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.ColAlbum = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.ColGenre = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.colFilePath = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.mediaPlayer = new AxWMPLib.AxWindowsMediaPlayer();
+            this.btnAddSong = new System.Windows.Forms.Button();
+            this.lblNumTracks = new System.Windows.Forms.Label();
+            this.btnDeleteSong = new System.Windows.Forms.Button();
             this.btnPlaySong = new System.Windows.Forms.Button();
             this.btnStop = new System.Windows.Forms.Button();
+            this.mediaPlayer = new AxWMPLib.AxWindowsMediaPlayer();
             ((System.ComponentModel.ISupportInitialize)(this.picCoverArt)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dgvSongs)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.mediaPlayer)).BeginInit();
@@ -59,8 +59,7 @@
             this.lblName.AutoSize = true;
             this.lblName.Font = new System.Drawing.Font("Book Antiqua", 29.25F);
             this.lblName.ForeColor = System.Drawing.Color.Black;
-            this.lblName.Location = new System.Drawing.Point(309, 9);
-            this.lblName.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblName.Location = new System.Drawing.Point(412, 11);
             this.lblName.Name = "lblName";
             this.lblName.Size = new System.Drawing.Size(330, 59);
             this.lblName.TabIndex = 0;
@@ -71,8 +70,7 @@
             this.lblDateCreated.AutoSize = true;
             this.lblDateCreated.Font = new System.Drawing.Font("Book Antiqua", 12F);
             this.lblDateCreated.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(153)))), ((int)(((byte)(174)))));
-            this.lblDateCreated.Location = new System.Drawing.Point(386, 55);
-            this.lblDateCreated.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblDateCreated.Location = new System.Drawing.Point(515, 68);
             this.lblDateCreated.Name = "lblDateCreated";
             this.lblDateCreated.Size = new System.Drawing.Size(128, 24);
             this.lblDateCreated.TabIndex = 1;
@@ -80,9 +78,10 @@
             // 
             // picCoverArt
             // 
-            this.picCoverArt.Location = new System.Drawing.Point(12, 99);
+            this.picCoverArt.Location = new System.Drawing.Point(16, 122);
+            this.picCoverArt.Margin = new System.Windows.Forms.Padding(4);
             this.picCoverArt.Name = "picCoverArt";
-            this.picCoverArt.Size = new System.Drawing.Size(208, 172);
+            this.picCoverArt.Size = new System.Drawing.Size(277, 212);
             this.picCoverArt.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.picCoverArt.TabIndex = 2;
             this.picCoverArt.TabStop = false;
@@ -92,10 +91,10 @@
             this.btnSelectCoverImage.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(43)))), ((int)(((byte)(45)))), ((int)(((byte)(66)))));
             this.btnSelectCoverImage.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnSelectCoverImage.ForeColor = System.Drawing.Color.White;
-            this.btnSelectCoverImage.Location = new System.Drawing.Point(9, 316);
-            this.btnSelectCoverImage.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnSelectCoverImage.Location = new System.Drawing.Point(12, 389);
+            this.btnSelectCoverImage.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnSelectCoverImage.Name = "btnSelectCoverImage";
-            this.btnSelectCoverImage.Size = new System.Drawing.Size(84, 40);
+            this.btnSelectCoverImage.Size = new System.Drawing.Size(112, 49);
             this.btnSelectCoverImage.TabIndex = 3;
             this.btnSelectCoverImage.Text = "Select Cover Image";
             this.btnSelectCoverImage.UseVisualStyleBackColor = false;
@@ -107,10 +106,10 @@
             this.BtnSaveCover.Enabled = false;
             this.BtnSaveCover.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.BtnSaveCover.ForeColor = System.Drawing.Color.White;
-            this.BtnSaveCover.Location = new System.Drawing.Point(119, 316);
-            this.BtnSaveCover.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.BtnSaveCover.Location = new System.Drawing.Point(159, 389);
+            this.BtnSaveCover.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.BtnSaveCover.Name = "BtnSaveCover";
-            this.BtnSaveCover.Size = new System.Drawing.Size(86, 40);
+            this.BtnSaveCover.Size = new System.Drawing.Size(115, 49);
             this.BtnSaveCover.TabIndex = 4;
             this.BtnSaveCover.Text = "Save Cover Image";
             this.BtnSaveCover.UseVisualStyleBackColor = false;
@@ -120,10 +119,10 @@
             // 
             this.btnBack.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(43)))), ((int)(((byte)(45)))), ((int)(((byte)(66)))));
             this.btnBack.ForeColor = System.Drawing.Color.White;
-            this.btnBack.Location = new System.Drawing.Point(835, 316);
-            this.btnBack.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnBack.Location = new System.Drawing.Point(1113, 389);
+            this.btnBack.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnBack.Name = "btnBack";
-            this.btnBack.Size = new System.Drawing.Size(86, 40);
+            this.btnBack.Size = new System.Drawing.Size(115, 49);
             this.btnBack.TabIndex = 5;
             this.btnBack.Text = "Back";
             this.btnBack.UseVisualStyleBackColor = false;
@@ -146,47 +145,16 @@
             dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
             dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
             this.dgvSongs.DefaultCellStyle = dataGridViewCellStyle1;
-            this.dgvSongs.Location = new System.Drawing.Point(293, 99);
+            this.dgvSongs.Location = new System.Drawing.Point(391, 122);
+            this.dgvSongs.Margin = new System.Windows.Forms.Padding(4);
             this.dgvSongs.MultiSelect = false;
             this.dgvSongs.Name = "dgvSongs";
             this.dgvSongs.ReadOnly = true;
             this.dgvSongs.RowHeadersWidth = 51;
             dataGridViewCellStyle2.ForeColor = System.Drawing.Color.Black;
             this.dgvSongs.RowsDefaultCellStyle = dataGridViewCellStyle2;
-            this.dgvSongs.Size = new System.Drawing.Size(443, 212);
+            this.dgvSongs.Size = new System.Drawing.Size(591, 261);
             this.dgvSongs.TabIndex = 6;
-            // 
-            // btnAddSong
-            // 
-            this.btnAddSong.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(43)))), ((int)(((byte)(45)))), ((int)(((byte)(66)))));
-            this.btnAddSong.Location = new System.Drawing.Point(293, 316);
-            this.btnAddSong.Name = "btnAddSong";
-            this.btnAddSong.Size = new System.Drawing.Size(75, 39);
-            this.btnAddSong.TabIndex = 7;
-            this.btnAddSong.Text = "Add Song";
-            this.btnAddSong.UseVisualStyleBackColor = false;
-            this.btnAddSong.Click += new System.EventHandler(this.btnAddSong_Click);
-            // 
-            // lblNumTracks
-            // 
-            this.lblNumTracks.AutoSize = true;
-            this.lblNumTracks.ForeColor = System.Drawing.Color.Black;
-            this.lblNumTracks.Location = new System.Drawing.Point(635, 314);
-            this.lblNumTracks.Name = "lblNumTracks";
-            this.lblNumTracks.Size = new System.Drawing.Size(110, 15);
-            this.lblNumTracks.TabIndex = 8;
-            this.lblNumTracks.Text = "Number of Tracks: ";
-            // 
-            // btnDeleteSong
-            // 
-            this.btnDeleteSong.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(43)))), ((int)(((byte)(45)))), ((int)(((byte)(66)))));
-            this.btnDeleteSong.Location = new System.Drawing.Point(374, 318);
-            this.btnDeleteSong.Name = "btnDeleteSong";
-            this.btnDeleteSong.Size = new System.Drawing.Size(75, 39);
-            this.btnDeleteSong.TabIndex = 9;
-            this.btnDeleteSong.Text = "Delete Song";
-            this.btnDeleteSong.UseVisualStyleBackColor = false;
-            this.btnDeleteSong.Click += new System.EventHandler(this.btnDeleteSong_Click);
             // 
             // ColTitle
             // 
@@ -229,21 +197,48 @@
             this.colFilePath.Visible = false;
             this.colFilePath.Width = 125;
             // 
-            // mediaPlayer
+            // btnAddSong
             // 
-            this.mediaPlayer.Enabled = true;
-            this.mediaPlayer.Location = new System.Drawing.Point(792, 84);
-            this.mediaPlayer.Name = "mediaPlayer";
-            this.mediaPlayer.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("mediaPlayer.OcxState")));
-            this.mediaPlayer.Size = new System.Drawing.Size(128, 136);
-            this.mediaPlayer.TabIndex = 10;
+            this.btnAddSong.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(43)))), ((int)(((byte)(45)))), ((int)(((byte)(66)))));
+            this.btnAddSong.Location = new System.Drawing.Point(391, 389);
+            this.btnAddSong.Margin = new System.Windows.Forms.Padding(4);
+            this.btnAddSong.Name = "btnAddSong";
+            this.btnAddSong.Size = new System.Drawing.Size(100, 48);
+            this.btnAddSong.TabIndex = 7;
+            this.btnAddSong.Text = "Add Song";
+            this.btnAddSong.UseVisualStyleBackColor = false;
+            this.btnAddSong.Click += new System.EventHandler(this.btnAddSong_Click);
+            // 
+            // lblNumTracks
+            // 
+            this.lblNumTracks.AutoSize = true;
+            this.lblNumTracks.ForeColor = System.Drawing.Color.Black;
+            this.lblNumTracks.Location = new System.Drawing.Point(847, 386);
+            this.lblNumTracks.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblNumTracks.Name = "lblNumTracks";
+            this.lblNumTracks.Size = new System.Drawing.Size(120, 16);
+            this.lblNumTracks.TabIndex = 8;
+            this.lblNumTracks.Text = "Number of Tracks: ";
+            // 
+            // btnDeleteSong
+            // 
+            this.btnDeleteSong.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(43)))), ((int)(((byte)(45)))), ((int)(((byte)(66)))));
+            this.btnDeleteSong.Location = new System.Drawing.Point(715, 391);
+            this.btnDeleteSong.Margin = new System.Windows.Forms.Padding(4);
+            this.btnDeleteSong.Name = "btnDeleteSong";
+            this.btnDeleteSong.Size = new System.Drawing.Size(100, 48);
+            this.btnDeleteSong.TabIndex = 9;
+            this.btnDeleteSong.Text = "Delete Song";
+            this.btnDeleteSong.UseVisualStyleBackColor = false;
+            this.btnDeleteSong.Click += new System.EventHandler(this.btnDeleteSong_Click);
             // 
             // btnPlaySong
             // 
             this.btnPlaySong.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(43)))), ((int)(((byte)(45)))), ((int)(((byte)(66)))));
-            this.btnPlaySong.Location = new System.Drawing.Point(455, 318);
+            this.btnPlaySong.Location = new System.Drawing.Point(499, 391);
+            this.btnPlaySong.Margin = new System.Windows.Forms.Padding(4);
             this.btnPlaySong.Name = "btnPlaySong";
-            this.btnPlaySong.Size = new System.Drawing.Size(75, 39);
+            this.btnPlaySong.Size = new System.Drawing.Size(100, 48);
             this.btnPlaySong.TabIndex = 11;
             this.btnPlaySong.Text = "Play Song";
             this.btnPlaySong.UseVisualStyleBackColor = false;
@@ -252,19 +247,32 @@
             // btnStop
             // 
             this.btnStop.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(43)))), ((int)(((byte)(45)))), ((int)(((byte)(66)))));
-            this.btnStop.Location = new System.Drawing.Point(536, 318);
+            this.btnStop.Location = new System.Drawing.Point(607, 391);
+            this.btnStop.Margin = new System.Windows.Forms.Padding(4);
             this.btnStop.Name = "btnStop";
-            this.btnStop.Size = new System.Drawing.Size(75, 39);
+            this.btnStop.Size = new System.Drawing.Size(100, 48);
             this.btnStop.TabIndex = 12;
             this.btnStop.Text = "Stop Song";
             this.btnStop.UseVisualStyleBackColor = false;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // mediaPlayer
+            // 
+            this.mediaPlayer.Enabled = true;
+            this.mediaPlayer.Location = new System.Drawing.Point(792, 84);
+            this.mediaPlayer.Margin = new System.Windows.Forms.Padding(4);
+            this.mediaPlayer.Name = "mediaPlayer";
+            this.mediaPlayer.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("mediaPlayer.OcxState")));
+            this.mediaPlayer.Size = new System.Drawing.Size(128, 136);
+            this.mediaPlayer.TabIndex = 10;
+            this.mediaPlayer.Enter += new System.EventHandler(this.mediaPlayer_Enter);
             // 
             // frmPlaylist
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(237)))), ((int)(((byte)(242)))), ((int)(((byte)(244)))));
-            this.ClientSize = new System.Drawing.Size(932, 393);
+            this.ClientSize = new System.Drawing.Size(1243, 484);
             this.Controls.Add(this.btnStop);
             this.Controls.Add(this.btnPlaySong);
             this.Controls.Add(this.mediaPlayer);
@@ -281,7 +289,7 @@
             this.ForeColor = System.Drawing.Color.White;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.MaximizeBox = false;
             this.Name = "frmPlaylist";
             this.Opacity = 0.99D;
