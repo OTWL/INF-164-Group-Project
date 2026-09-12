ce084d03d22ceaa2360773b3cebb0f29d196f9ea
Chris <82661103+OTWL@users.noreply.github.com>
2026-09-12 14:04:49 +0200

    Refactor: move Song, Playlist, User to separate files
    Separated Song, Playlist, and User classes from Global.cs into
    individual files for better organization. Updated all references
    to use direct class names. Modified GroupProject.csproj to
    include new files. Simplified Global.cs and improved some
    exception messages. Enhances maintainability and clarity.

---
+++ Playlist.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroupProject
+{
+    [Serializable]
+    public class Playlist
+    {
+        //One playlist name
+        private string mTitle;
+        private string mDateOfCreation;
+        private string mCoverArtPath;
+        private bool mFavourite;
+        private string mUsername;
+
+        //Playlist song list
+        private List<Song> mSongs = new List<Song>();
+        public string Username
+        {
+            get { return mUsername; }
+            set { mUsername = value; }
+
+        }
+
+        //Constructor
+        public Playlist(string title, string CoverArtPath, string UserName, string DateOfCreation = "", bool isFavourite = false)
+        {
+            mTitle = title;
+            mCoverArtPath = CoverArtPath;
+            mFavourite = isFavourite;
+
+            mUsername = UserName;
+
+            if (DateOfCreation == "")
+            {
+                mDateOfCreation = DateTime.Now.ToString("yyyy/MM/dd");
+            }
+            else
+            {
+                mDateOfCreation = DateOfCreation;
+            }
+            //USE THIS DATE FORMAT OR Problems will occur
+        }
+
+        public void Remove(Song song)
+        {
+            mSongs.Remove(song);
+        }
+        public string GetTitle()
+        {
+            return mTitle;
+        }
+
+        public string GetDateOfCreation()
+        {
+            return mDateOfCreation;
+        }
+
+        public string getCoverPath()
+        {
+            return mCoverArtPath;
+        }
+
+        public void SetCoverpath(string CoverPath)
+        {
+            mCoverArtPath = CoverPath;
+        }
+
+        public void AddSong(Song song)
+        {
+            mSongs.Add(song);
+        }
+
+        public List<Song> GetSongs()
+        {
+            return mSongs;
+        }
+
+    }
+}
+
---
+++ Song.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroupProject
+{
+    [Serializable]
+    public class Song
+    {
+        private string mTitle;
+        private string mArtist;
+        private string mAlbum;
+        private string mGenre;
+        private string mFilePath;
+
+        public string Title
+        {
+            get { return mTitle; }
+            set { mTitle = value; }
+        }
+
+
+        public string Artist
+        {
+            get { return mArtist; }
+            set { mArtist = value; }
+        }
+
+        public string Album
+        {
+            get { return mAlbum; }
+            set { mAlbum = value; }
+        }
+
+        public string Genre
+        {
+            get { return mGenre; }
+            set { mGenre = value; }
+        }
+
+        public string FilePath
+        {
+            get { return mFilePath; }
+            set { mFilePath = value; }
+        }
+
+        //Constructor
+        public Song(string title, string artist, string album, string genere, string filePath)
+        {
+            mTitle = title;
+            mArtist = artist;
+            mAlbum = album;
+            mGenre = genere;
+            //Always assign FilePath as the current user to keep track of ownership
+            mFilePath = filePath;
+
+        }
+    };
+
+}
+
